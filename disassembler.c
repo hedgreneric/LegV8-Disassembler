a900cf98ab89ff4ae3e3ed4b0592ac8869e27366
ehedgren <ehedgren@pyrite-n2.cs.iastate.edu>
2023-04-20 13:43:53 -0500

    I dont know what i changed. I changed something

--- disassembler.c
+++ disassembler.c
@@ -1,3 +1,6 @@
+// Eric Hedgren - ehedgren
+// Dominik Chally - drchally TODO check that I put the right netid
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -12,18 +15,18 @@
  * To work around this you can run this in the terminal in pyrite (a Linux OS)
  */
 
-#include <inttypes.h> // used to print uint32_t value
+void decode(uint32_t*, int*); // declaring the decode function so that I can use it after the main method
 
 int main(int argc, char *argv[]){
-    bool binary; // false
+    bool binary;
     int fd;
     uint32_t* program;
     struct stat buf;
     int* bprogram;
     int i;
 
-    // TODO: get value of binary by finding out if file is a binary file. Probaly will have to clear out the hex values
-    binary = true;
+    // TODO: get value of binary by finding out if file is a binary file
+    binary = true; // we may not have to check if it is a binary file
     if (binary){ // checks if the file is a binary file
         fd = open(argv[1], O_RDONLY); // opens the specified file given from the cmd
         fstat(fd, &buf); // gets the size of the file fills struct stat with the pointer buf
@@ -33,10 +36,13 @@ int main(int argc, char *argv[]){
         for (i = 0; i < (buf.st_size / 4); i++){ // iterates through each value
             program[i] = be32toh(program[i]); // reads 32-bit value and converts it from big-endian to byte order
             //decode(program[i], bprogram + i); // we have to do this method, but it decodes it
-            printf("%x\n", program[i]);
         }
 
         //emulate(bprogram, buf->st_size / 4, &m); // I guess this runs the code that we decode
     }
     return 0;
+}
+
+void decode(uint32_t* program, int* bprogram) {
+
 }
